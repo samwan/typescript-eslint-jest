43f3abd510db68bb6d745e53c8d412f58bd5a7b2
Samuel Wan <sam@samuelwan.com>
2020-10-25 16:43:24 -0700

    Fix syntax error in eslintrc to use  again. Set eslintrc env to node to prevent vscode from highlighting  with  warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,10 @@
-export default {
+module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  env: {
+    'node': true
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
